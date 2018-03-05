fb294611b924b2001a5264e3da12fb8bef5fa6f9
losiz17 <riokam.11.12@docomo.ne.jp>
2018-03-05 23:10:03 +0900

    first commit

--- Count/ViewController.swift
+++ Count/ViewController.swift
@@ -9,6 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var number: Int = 0
+    @IBOutlet var label: UILabel!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +23,32 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func plus(){
+        number = number + 1
+        label.text = String(number)
+        //label.textColor = UIColor.blue
+    }
+    @IBAction func minus(){
+        number = number - 1
+        label.text = String(number)
+        //label.textColor = UIColor.red
+    }
+    @IBAction func kakeru(){
+        number = number * 2
+        label.text = String(number)
+        //label.textColor = UIColor.green
+    }
+    @IBAction func waruru(){
+        number = number / 2
+        label.text = String(number)
+        //label.textColor = UIColor.yellow
+    }
+    @IBAction func clrear(){
+        number = 0
+        label.text = String(number)
+        //label.textColor = UIColor.yellow
     }
 
 
